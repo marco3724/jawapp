21d4285da1341cdd9e20c2c5ce1ccde1e41d10c6
marco <marcowang05@gmail.com>
2022-05-25 11:23:19 +0200

    ok mapView

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -9,7 +9,7 @@
       <div class="choice"> <i class="fa-solid fa-cloud-rain " @click="select(2)"></i></div>
       <div class="choice"> <i class="fa-solid fa-snowflake" @click="select(3)"></i></div>
    </div>
-   <button>segnala</button>
+   <button @click="signal">segnala</button>
   </div>
   <div class="form" v-else>
     <div class="error">
@@ -17,7 +17,7 @@
     </div>
   </div>
   <GoogleMap api-key="AIzaSyDBm40JV-OqOyW_wTjclonA40i-C934Mv4" style="width: 100%; height: 65vh" :center="center" :zoom="6">
-    <Marker :options="{ position: mark.coords }" v-for="mark in markers" :key="mark">
+    <Marker :options="{ position: mark.coords }" v-for="mark in markers" :key="mark" @click="getInfo(mark.city,mark.index)">
       <InfoWindow>
         <div id="contet">
           <div id="siteNotice"></div>
@@ -39,6 +39,7 @@
 /* eslint-disable no-undef */
 import { defineComponent } from 'vue'
 import { GoogleMap, Marker, InfoWindow } from 'vue3-google-map'
+import http from '../http-common'
 // import { geoLocation } from '../geoLocation.js'
 export default defineComponent({
   components: { GoogleMap, Marker, InfoWindow },
@@ -81,6 +82,35 @@ export default defineComponent({
       for (let j = 0; j < 4; j++) {
         if (j === i) { c[j].className = 'choice selected' } else { c[j].className = 'choice' }
       }
+    },
+    getInfo (city, key) {
+      http.post('signal/get', {
+        city: city
+      }, {
+        headers: {
+          'x-access-token': localStorage.getItem('accessToken')
+        }
+      }).then(res => {
+        console.log(res)
+        this.markers[key].signal[0] = res.data.sun
+        this.markers[key].signal[1] = res.data.cloud
+        this.markers[key].signal[2] = res.data.rain
+        this.markers[key].signal[3] = res.data.snow
+      })
+        .catch(err => console.log(err))
+    },
+    signal () {
+      http.post('signal/add', {
+        city: this.city,
+        signal: this.index + 1
+      }, {
+        headers: {
+          'x-access-token': localStorage.getItem('accessToken')
+        }
+      }).then(res => {
+        console.log(res)
+      })
+        .catch(err => console.log(err))
     }
     // async getCity (currPos) {
     //   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${currPos.lat},${currPos.lng}&key=AIzaSyBtNRVr8IbLg1JMNJKyi2T4F334JedSH6g`
@@ -101,46 +131,55 @@ export default defineComponent({
       errorStr: null,
       markers: [
         {
+          index: 0,
           coords: { lat: 41.902782, lng: 12.496366 },
           city: 'Roma',
           signal: [0, 0, 0, 0]
         },
         {
+          index: 1,
           coords: { lat: 39.223843, lng: 9.121661 },
           city: 'Cagliari',
           signal: [0, 0, 0, 0]
         },
         {
+          index: 2,
           coords: { lat: 43.769562, lng: 11.255814 },
           city: 'Firenze',
           signal: [0, 0, 0, 0]
         },
         {
+          index: 3,
           coords: { lat: 45.4613, lng: 9.1595 },
           city: 'Milano',
           signal: [0, 0, 0, 0]
         },
         {
+          index: 4,
           coords: { lat: 45.068371, lng: 7.683070 },
           city: 'Torino',
           signal: [0, 0, 0, 0]
         },
         {
+          index: 5,
           coords: { lat: 45.440845, lng: 12.315515 },
           city: 'Venezia',
           signal: [0, 0, 0, 0]
         },
         {
+          index: 6,
           coords: { lat: 38.115662, lng: 13.361470 },
           city: 'Palermo',
           signal: [0, 0, 0, 0]
         },
         {
+          index: 7,
           coords: { lat: 41.125912, lng: 16.872110 },
           city: 'Bari',
           signal: [0, 0, 0, 0]
         },
         {
+          index: 8,
           coords: { lat: 40.8359336, lng: 14.2487826 },
           city: 'Napoli',
           signal: [0, 0, 0, 0]
